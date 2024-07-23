d62594be8750fd00601bb67e40d20cffd5773e42
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2024-07-23 14:52:01 -0300

    Criando primeiros atoms e marcando interface

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,54 @@
-import Link from 'next/link';
+import React from 'react';
+
+interface ListProps {
+    items: string[];
+}
+
+const List: React.FC<ListProps> = ({ items }) => (
+    <ul data-testid="list" className="gap-pequeno flex">
+        {items.map((item) => (
+            <li className="text-white" key={item}>
+                {item}
+            </li>
+        ))}
+    </ul>
+);
+
+const Logo: React.FC = () => (
+    <h1 data-testid="logo" className="text-white text-grande font-semiBold">
+        RealState
+    </h1>
+);
+
+const MenuPrincipal: React.FC = () => {
+    const items = ['Inicio', 'Fotos', 'Descrição'];
+
+    return (
+        <nav
+            data-testid="menu-principal"
+            className="bg-azulForte h-[70px] w-full justify-between items-center flex px-medio"
+        >
+            <div>
+                <Logo />
+            </div>
+
+            <div>
+                <List items={items} />
+            </div>
+        </nav>
+    );
+};
+
+const Header: React.FC = () => (
+    <header>
+        <MenuPrincipal />
+    </header>
+);
 
 export default function Home() {
     return (
-        <div>
-            <h1>Home</h1>
-            <Link href="/about">About</Link>
-        </div>
+        <>
+            <Header />
+        </>
     );
 }
