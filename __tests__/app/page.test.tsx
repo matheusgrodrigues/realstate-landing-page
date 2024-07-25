9d49f080cd7b41095413eebbc66decedeafec107
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2024-07-25 16:37:38 -0300

    Criando funcionalidade toggle e criando testes

--- __tests__/app/page.test.tsx
+++ __tests__/app/page.test.tsx
@@ -1,37 +1,88 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import Page from '@/app/page';
 
-describe('Deve renderizar a Home corretamente', () => {
+describe('Deve renderizar a Home', () => {
     beforeEach(() => render(<Page />));
 
-    describe('Deve renderizar o MenuPrincipal corretamente', () => {
-        it('Deve renderizar o MenuPrincipal', () => {
-            const menu = screen.getByTestId('menu-principal');
+    describe('Deve renderizar o Header', () => {
+        describe('Deve renderizar o MenuPrincipal', () => {
+            it('Deve renderizar o MenuPrincipal na tela', () => {
+                const menu = screen.getByTestId('menu-principal');
 
-            expect(menu).toBeInTheDocument();
+                expect(menu).toBeInTheDocument();
+            });
+
+            it('Deve renderizar o botão de abrir o menu no Mobile', () => {
+                const btn = screen.getByTestId('btn-open-menu');
+
+                expect(btn).toBeInTheDocument();
+            });
+
+            it('Deve renderizar a logo', () => {
+                const logo = screen.getByTestId('logo');
+
+                expect(logo).toBeInTheDocument();
+            });
+
+            it('Dever renderizar os links', () => {
+                const list = screen.getByTestId('list');
+                const items = list.querySelectorAll('li');
+
+                expect(list).toBeInTheDocument();
+                expect(items).toHaveLength(3);
+                ['Inicio', 'Fotos', 'Descrição'].forEach((item, key) =>
+                    expect(items[key]).toHaveTextContent(`${item}`)
+                );
+            });
         });
 
-        it('Deve renderizar o botão de abrir o menu no Mobile', () => {
-            const btn = screen.getByTestId('btn-open-menu');
+        describe('Deve renderizar a Seção Principal', () => {
+            describe('Deve renderizar a "Galeria de Fotos Principal"', () => {
+                it('Deve renderizar o component Fotos ao clicar no botão "Fotos"', () => {
+                    const btnOpenFotos = screen.getByTestId('btn-open-fotos');
+                    fireEvent.click(btnOpenFotos);
 
-            expect(btn).toBeInTheDocument();
-        });
+                    const video = screen.queryByTestId('component-video');
+                    const fotos = screen.queryByTestId('component-fotos');
+                    const mapa = screen.queryByTestId('component-mapa');
 
-        it('Deve renderizar a logo', () => {
-            const logo = screen.getByTestId('logo');
+                    expect(fotos).toBeInTheDocument();
+                    expect(video).not.toBeInTheDocument();
+                    expect(mapa).not.toBeInTheDocument();
+                });
+            });
 
-            expect(logo).toBeInTheDocument();
-        });
+            describe('Deve renderizar o component "Vídeo"', () => {
+                it('Deve renderizar o component Vídeo ao clicar no botão "Vídeo"', () => {
+                    const btnOpenVideo = screen.getByTestId('btn-open-video');
+                    fireEvent.click(btnOpenVideo);
 
-        it('Dever renderizar os links', () => {
-            const list = screen.getByTestId('list');
-            const items = list.querySelectorAll('li');
+                    const video = screen.getByTestId('component-video');
+                    const fotos = screen.queryByTestId('component-fotos');
+                    const mapa = screen.queryByTestId('component-mapa');
 
-            expect(list).toBeInTheDocument();
-            expect(items).toHaveLength(3);
-            ['Inicio', 'Fotos', 'Descrição'].forEach((item, key) => expect(items[key]).toHaveTextContent(`${item}`));
+                    expect(video).toBeInTheDocument();
+                    expect(fotos).not.toBeInTheDocument();
+                    expect(mapa).not.toBeInTheDocument();
+                });
+            });
+
+            describe('Deve renderizar o component "Mapa"', () => {
+                it('Deve renderizar o Mapa ao clicar no botão "Mapa"', () => {
+                    const btnOpenMapa = screen.getByTestId('btn-open-mapa');
+                    fireEvent.click(btnOpenMapa);
+
+                    const video = screen.queryByTestId('component-video');
+                    const fotos = screen.queryByTestId('component-fotos');
+                    const mapa = screen.getByTestId('component-mapa');
+
+                    expect(fotos).not.toBeInTheDocument();
+                    expect(video).not.toBeInTheDocument();
+                    expect(mapa).toBeInTheDocument();
+                });
+            });
         });
     });
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { default as NextLink } from 'next/link';
-import React, { forwardRef, useEffect, useRef, useState } from 'react';
+import React, { forwardRef, useCallback, useEffect, useState, useRef } from 'react';
 import { BuildingLibraryIcon, Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
 
 import debounce from './lib/util/debounce';
@@ -102,58 +102,153 @@ const MenuPrincipal: React.FC = () => {
     return (
         <nav
             data-testid="menu-principal"
-            className={`${menuBgColor} fixed top-[0] h-[4rem] w-full justify-between items-center flex px-medio ease-linear duration-200`}
+            className={`${menuBgColor} fixed top-[0] h-[4rem] w-full px-medio ease-linear duration-200`}
             ref={menuPrincipalRef}
         >
-            <div>
-                <Icon
-                    data-testid="logo"
-                    className={`text-grande font-semiBold w-[2rem] ${menuTextClassNames}`}
-                    icon="building-library"
-                />
-            </div>
+            <div className="w-full justify-between items-center flex container mx-auto">
+                <div>
+                    <Icon
+                        data-testid="logo"
+                        className={`text-grande font-semiBold w-[2rem] ${menuTextClassNames}`}
+                        icon="building-library"
+                    />
+                </div>
 
-            <div className="items-center flex">
-                <button
-                    data-testid="btn-open-menu"
-                    className={`outline-none size-extraMedio md:hidden`}
-                    onClick={() => {
-                        setMenuIsOpen(!menuIsOpen);
+                <div className="items-center flex">
+                    <button
+                        data-testid="btn-open-menu"
+                        className={`outline-none size-extraMedio md:hidden`}
+                        onClick={() => {
+                            setMenuIsOpen(!menuIsOpen);
 
-                        if (!menuHasOpened) {
-                            setMenuHasOpened(true);
-                        }
-                    }}
-                >
-                    {menuIsOpen ? (
-                        <Icon className={menuTextClassNames} icon="x-icon" />
-                    ) : (
-                        <Icon className={menuTextClassNames} icon="bars-3" />
-                    )}
-                </button>
+                            if (!menuHasOpened) {
+                                setMenuHasOpened(true);
+                            }
+                        }}
+                    >
+                        {menuIsOpen ? (
+                            <Icon className={menuTextClassNames} icon="x-icon" />
+                        ) : (
+                            <Icon className={menuTextClassNames} icon="bars-3" />
+                        )}
+                    </button>
 
-                <div
-                    className={`${menuBgColor} md:bg-transparent absolute md:relative top-[4rem] md:top-auto right-[0] md:right-auto w-3/4 md:w-auto p-medio md:block md:animate-none 
+                    <div
+                        className={`${menuBgColor} md:bg-transparent absolute md:relative top-[4rem] md:top-auto right-[0] md:right-auto w-3/4 md:w-auto p-medio md:block md:animate-none 
                     ${menuIsOpen ? 'block animate-slideIn' : menuHasOpened && !isResizing ? 'animate-slideOut' : 'hidden'}`}
-                >
-                    <ul data-testid="list" className={`gap-pequeno flex-col flex md:flex-row`}>
-                        {menu_links.map(({ displayName, path }) => (
-                            <li className={menuTextClassNames} key={displayName}>
-                                <Link href={{ pathname: path }}>{displayName}</Link>
-                            </li>
-                        ))}
-                    </ul>
+                    >
+                        <ul data-testid="list" className={`gap-pequeno flex-col flex md:flex-row`}>
+                            {menu_links.map(({ displayName, path }) => (
+                                <li className={menuTextClassNames} key={displayName}>
+                                    <Link href={{ pathname: path }}>{displayName}</Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
             </div>
         </nav>
     );
 };
 
-const Header: React.FC = () => (
-    <header className="relative">
-        <MenuPrincipal />
-    </header>
-);
+const fotos = [
+    {
+        legenda: 'Foto1',
+    },
+];
+const video = 'Video';
+const mapa = 'Mapa';
+
+const Header: React.FC = () => {
+    const [showComponent, setShowComponent] = useState({
+        video: false,
+        fotos: true,
+        mapa: false,
+    });
+
+    const handleShowFotos = useCallback(() => {
+        setShowComponent({
+            video: false,
+            fotos: true,
+            mapa: false,
+        });
+    }, []);
+
+    const handleShowVideo = useCallback(() => {
+        setShowComponent({
+            video: true,
+            fotos: false,
+            mapa: false,
+        });
+    }, []);
+
+    const handleShowMapa = useCallback(() => {
+        setShowComponent({
+            video: false,
+            fotos: false,
+            mapa: true,
+        });
+    }, []);
+
+    return (
+        <header className="relative">
+            <MenuPrincipal />
+
+            <div className="w-full items-center flex mt-[4rem] h-[30rem] border-solid border-2">
+                {showComponent.fotos && (
+                    <div
+                        data-testid="component-fotos"
+                        className="size-full bg-azulForte2 items-center justify-center flex"
+                    >
+                        <div>{fotos[0].legenda}</div>
+                    </div>
+                )}
+
+                {showComponent.video && (
+                    <div
+                        data-testid="component-video"
+                        className="size-full bg-azulForte items-center justify-center flex"
+                    >
+                        {video}
+                    </div>
+                )}
+
+                {showComponent.mapa && (
+                    <div
+                        data-testid="component-mapa"
+                        className="size-full bg-azulFeio items-center justify-center flex"
+                    >
+                        {mapa}
+                    </div>
+                )}
+            </div>
+
+            <div className="container mx-auto p-medio flex gap-pequeno">
+                <button
+                    data-testid="btn-open-fotos"
+                    className="bg-azulForte text-white font-bold px-pequeno h-[3rem] rounded-md"
+                    onClick={handleShowFotos}
+                >
+                    Fotos
+                </button>
+                <button
+                    data-testid="btn-open-video"
+                    className="bg-azulForte text-white font-bold px-pequeno h-[3rem] rounded-md"
+                    onClick={handleShowVideo}
+                >
+                    Vídeo
+                </button>
+                <button
+                    data-testid="btn-open-mapa"
+                    className="bg-azulForte text-white font-bold px-pequeno h-[3rem] rounded-md"
+                    onClick={handleShowMapa}
+                >
+                    Mapa
+                </button>
+            </div>
+        </header>
+    );
+};
 
 export default function Home() {
     return (
